31b6e59092def647f2c1260e86f0d48d2035ed5f
Niclas Hedhman <niclas@hedhman.org>
2015-01-30 18:02:37 +0800

    Test for RS-485.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,23 @@
 #include <stdio.h>
 #include <modbus.h>
 
-int
-main (void)
+void poll(void)
+{
+  modbus_t *mb;
+  uint16_t tab_reg[32];
+
+  mb = modbus_new_rtu("/dev/ttyS0", 9600, 'n', 8, 1 );
+
+  modbus_connect(mb);
+
+  /* Read 5 registers from the address 0 */
+  modbus_read_registers(mb, 0, 5, tab_reg);
+
+  modbus_close(mb);
+  modbus_free(mb);
+}
+
+int main (void)
 {
   int vermaj = libmodbus_version_major;
   int vermin = libmodbus_version_minor;
@@ -13,5 +28,8 @@ main (void)
   puts ("Hello World!");
   puts ("This is " PACKAGE_STRING ".");
   printf("Modbus library version: %i.%i.%i\n", vermaj, vermin, vermic);
+  poll();
   return 0;
 }
+
+
