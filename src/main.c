beb23e86b779d1e297847199bfcd898d5ae8fa8d
Niclas Hedhman <niclas@hedhman.org>
2015-02-02 12:09:05 +0800

    Debug output statements.

--- src/main.c
+++ src/main.c
@@ -6,16 +6,20 @@ void poll(void)
 {
   modbus_t *mb;
   uint16_t tab_reg[32];
-
+  printf("1");
   mb = modbus_new_rtu("/dev/ttyS0", 9600, 'n', 8, 1 );
+  printf("2");
   modbus_connect(mb);
+  printf("3");
   modbus_set_slave(mb, 1);
-
+  printf("4");
   /* Read 5 registers from the address 0 */
   modbus_read_registers(mb, 0, 5, tab_reg);
-
+  printf("6");
   modbus_close(mb);
+  printf("7");
   modbus_free(mb);
+  printf("8");
 }
 
 int main (void)
